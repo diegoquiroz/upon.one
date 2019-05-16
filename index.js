1043335999d795055f5b876873f766322bee5fc3
Arnav Singh <arnav010singh@gmail.com>
2019-05-16 17:52:33 +0530

    a

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
-var port = 9000
+var port = 8080
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
