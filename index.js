3405f5747ad9c4e8cf0e11175111ae748656e3cc
Arnav Singh <arnav010singh@gmail.com>
2019-05-17 15:43:17 +0530

    lazy good

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(express.static(__dirname__ + '/public'));
+// app.use(express.static(__dirname__ + '/public'));
 
 
 let db = require('./db.js')
