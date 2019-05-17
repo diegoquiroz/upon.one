44950edfe3df864c8a4272342bafdd3cc71aa99e
Arnav Singh <arnav010singh@gmail.com>
2019-05-17 15:25:54 +0530

    framing

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
 var express = require('express')
 var bodyParser = require('body-parser')
- 
 var app = express()
- 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
- 
-var port = process.env.PORT || 8080
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+var port = process.env.PORT || 8080
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(express.static(__dirname__ + '/public'));
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+let db = require('./db.js')
+let app_set = db.apps
+let peer_set = db.peers
+
+
+var frameHtml
+
+fetch('serverination.js').then(data =>{
+  frameHtml = '<html><script>'+data+'</script> </html>'
+})
+
+
+
+app.get('/:id/:data', (req, res) => {
+
+  if (req.params.data == null) {
+    res.send(frameHtml)
+  }else{
+    app_set.find({name:req.params.id}).then( data=>{
+      res.send(data.seed.id)
+    })    
+  }  
+})
+
+
 
 app.post('/', (req, res) => {
 
-  // let toBsaved = req.query;
-  res.status(200).send('hey')
-  // res.send('data saved')
+  var newapp = new app_set{
+    name:req.body.name,
+    password:req.body.password,
+    version:0,
+    seed:req.body.peerid
+  }
 
-});
+  newapp.save(error=>{
+    if (error) throw err;
+    res.send('hosted')
+  })
 
-
+})
 
 
 
