b7390034fdeac5a95e47121d5cf21d9694648b04
Arnav Singh <arnav010singh@gmail.com>
2019-10-19 10:10:59 +0530

    fx 2 pm

--- loader.js
+++ loader.js
@@ -63,7 +63,7 @@ let loader = {
 
                 if(mama) mama = mama.getAttribute('app_name')
                 console.log('post message received',e.data, mama,e.origin )
-                if(e.origin !== 'https://upon.one' && e.origin !== 'http://upon.one' && e.origin !== null  && e.origin.indexOf('localhost') === -1 && e.origin !== 'http://www.upon.one' && e.origin !== 'https://www.upon.one'){ return; } 
+                if(e.origin !== 'https://upon.one' && e.origin !== 'http://upon.one' && e.origin.indexOf('file://') === -1  && e.origin.indexOf('localhost') === -1 && e.origin !== 'http://www.upon.one' && e.origin !== 'https://www.upon.one'){ return; } 
                 // alert(e.data);
                 console.log('post message approved',e.data)
 
